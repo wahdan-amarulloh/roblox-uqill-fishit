fc44cc9bb48ec4a02a79c619c9d5806726952554
syauqi1717 <muhammad.syauqi@ist.id>
2025-12-30 08:21:07 +0700

    fix: version to 4.3.1

--- uqill.lua
+++ uqill.lua
@@ -1898,7 +1898,7 @@ end
 
 local Window = WindUI:CreateWindow({ Title = "UQiLL", Icon = "chess-king", Author = "by UQi", Transparent = true })
 Window.Name = GUI_NAMES.Main 
-Window:Tag({ Title = "v.4.3.0", Icon = "github", Color = Color3.fromHex("#30ff6a"), Radius = 0 })
+Window:Tag({ Title = "v.4.3.1", Icon = "github", Color = Color3.fromHex("#30ff6a"), Radius = 0 })
 Window:SetToggleKey(Enum.KeyCode.H)
 
 local TabPlayer = Window:Tab({ Title = "Player Setting", Icon = "user" })
